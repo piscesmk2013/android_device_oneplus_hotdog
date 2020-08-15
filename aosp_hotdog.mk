a22f8a8240e471f2dc26f5e412150a6f4078889a
Just Pisces <justpisces2011@gmail.com>
2020-08-16 02:11:05 +0800

    hotdog: Add ro.product.odm.model to odm property blacklist

--- aosp_hotdog.mk
+++ aosp_hotdog.mk
@@ -45,6 +45,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus7TPro \
     PRIVATE_BUILD_DESC="OnePlus7TPro_EEA-user 10 QKQ1.190716.003 2007240000 release-keys"
 
+PRODUCT_ODM_PROPERTY_BLACKLIST += \
+    ro.product.odm.model
+
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.system.model
 
